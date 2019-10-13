7a013835f6f45fcbd3751182aedbf3a2a3c88d02
SXL5519 <shaoxinlin5519@163.com>
2019-10-13 14:53:22 +0800

    molotov 学习

---
+++ performance/Molotov.py
@@ -0,0 +1,22 @@
+from molotov import scenario
+
+
+_API = 'https://activity.godteam.net/redPond/redPondRollData'
+API='https://activity.godteam.net/redPond/redPondGoods'
+payload={'page': 1,'rows': 10}
+
+
+@scenario(weight=40)
+async def scenario_one(session):
+    async with session.post(API,json=payload) as resp:
+        res = await resp.json()
+        assert res['success'] == 'true'
+        assert resp.status == 200
+
+
+@scenario(weight=60)
+async def scenario_two(session):
+    async with session.get(_API) as resp:
+        r=await resp.json()##读取接口返回值
+        print(r)
+        assert resp.status == 200
