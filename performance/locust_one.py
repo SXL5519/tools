fc1861ed7fd69ac7ba3f36695a1deb7df9ba634e
SXL5519 <shaoxinlin5519@163.com>
2019-10-14 15:14:07 +0800

    添加post请求

--- performance/locust_one.py
+++ performance/locust_one.py
@@ -3,9 +3,17 @@ from locust import HttpLocust, TaskSet, task
 # 定义用户行为
 class UserBehavior(TaskSet):
 
-    @task
-    def baidu_index(self):
-        self.client.get("/")
+    # @task(1)
+    # def test(self):
+    #     self.client.get("/")
+
+    @task(5)
+    def test(self):
+        self.client.post("/ajgsapp/merchant/getMerchant",{'merchantId': '5d921eee4aa4239f262ebe5e','isNew': 'true'})
+
+    @task()
+    def test(self):
+        self.client.post("/ajgsapp/index/getMayYouLike", {'page': 1,'size': 8})
 
 
 class WebsiteUser(HttpLocust):
